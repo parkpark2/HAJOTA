5e238cc80b834900126278a65f6a7499a780fa6f
cjh1205 <cjh1205@gmail.com>
2017-06-25 16:17:43 +0900

    HY 컨트롤러 수정

--- HAJOTA/src/main/java/com/typeof/hajota/rooms/model/HY_RoomsDAO.java
+++ HAJOTA/src/main/java/com/typeof/hajota/rooms/model/HY_RoomsDAO.java
@@ -1,5 +1,7 @@
 package com.typeof.hajota.rooms.model;
 
+import java.util.HashMap;
+
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -9,11 +11,22 @@ import org.springframework.stereotype.Repository;
 @Repository
 public class HY_RoomsDAO implements InterRoomsDAO {
 
-//	===== #29. 의존객체 주입하기(DI : Dependency Injection) =====
-	@Autowired
-	private SqlSessionTemplate sqlsession;
+//   ===== #29. 의존객체 주입하기(DI : Dependency Injection) =====
+   @Autowired
+   private SqlSessionTemplate sqlsession;
 
-	
-	
+   public HashMap<String, Object> getlistdetail(String seq) {
+
+      HashMap<String, Object> listdetail = sqlsession.selectOne("HY_rooms.getlistdetail", seq);
+      
+      return listdetail;
+   }
+
+   public int addFile(HashMap<String, String> map) {
+      
+      int n = sqlsession.insert("HY_rooms.addFile", map);
+      
+      return n;
+   }
+   
 }
-
--- HAJOTA/src/main/java/com/typeof/hajota/rooms/service/HY_RoomsService.java
+++ HAJOTA/src/main/java/com/typeof/hajota/rooms/service/HY_RoomsService.java
@@ -1,14 +1,32 @@
 package com.typeof.hajota.rooms.service;
 
+import java.util.HashMap;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.typeof.hajota.rooms.model.RoomsDAO;
+import com.typeof.hajota.rooms.model.HY_RoomsDAO;
+
 
 @Service
 public class HY_RoomsService implements InterRoomsService {
 
-//	===== #31. 의존객체 주입하기(DI : Dependency Injection) =====
-	@Autowired
-	private RoomsDAO dao;
+//   ===== #31. 의존객체 주입하기(DI : Dependency Injection) =====
+   @Autowired
+   private HY_RoomsDAO dao;
+   
+   public HashMap<String, Object> getlistdetail(String seq) {
+
+      HashMap<String, Object> listdetail = dao.getlistdetail(seq);
+      
+      return listdetail;
+   }
+
+   public int addFile(HashMap<String, String> map) {
+
+      int n = dao.addFile(map);
+      
+      return n;
+   }
+   
 }
